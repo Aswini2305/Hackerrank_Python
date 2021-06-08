04eef08cbcec8b0cb87d2a6c2ad88c391eff489e
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2021-06-08 15:27:02 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -22,8 +22,8 @@ The function must return a Boolean value (True/False). Output is handled by the 
 def is_leap(year):
     leap = False
     if(year%4==0):
-        if(year%400==0):
-            if(year%100==0):
+        if(year%100==0):
+            if(year%400==0):
                 return True
             else:
                 return False
