ea0f6c5831c1c9c26cfef6f4b88ac803b1381ae9
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2021-06-09 20:29:28 +0530

    text wrapper using python

---
+++ Text_wrapper.py
@@ -0,0 +1,31 @@
+"""You are given a string s and width w.
+Your task is to wrap the string into a paragraph of width w.
+
+Function Description
+
+Complete the wrap function in the editor below.
+
+wrap has the following parameters:
+
+string string: a long string
+int max_width: the width to wrap to
+Returns
+
+string: a single string with newline characters ('\n') where the breaks should be
+Input Format
+
+The first line contains a string, s.
+The second line contains the width, max_width."""
+import textwrap
+
+def wrap(string, max_width):
+    result_string=""
+    result=textwrap.TextWrapper(max_width)
+    result_string=result.fill(text=string)
+    return result_string
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
